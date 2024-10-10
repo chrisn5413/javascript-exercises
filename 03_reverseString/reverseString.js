c258d88bfbbf0919800ccceb07c1bb7661781fed
Chris <chrisn5413@gmail.com>
2024-10-10 10:56:33 -0700

    completed exercise 3 but not as they expected will be commenting this out to do the other method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = "";
+    let stringLengthAsNegative = -1 * string.length;
+    for (let i = -1; i >= stringLengthAsNegative; i--) {
+        result += string.at(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
