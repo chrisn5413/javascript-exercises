75c1d04f9cb10fb60e8947aedac430a0c8baca78
Chris <chrisn5413@gmail.com>
2024-10-10 11:02:34 -0700

    resubmitted exercise 3, using string and array methods. Learned that split and join require separator strings.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,11 @@
 const reverseString = function(string) {
-    let result = "";
-    let stringLengthAsNegative = -1 * string.length;
-    for (let i = -1; i >= stringLengthAsNegative; i--) {
-        result += string.at(i);
-    }
-    return result;
+    // let result = "";
+    // let stringLengthAsNegative = -1 * string.length;
+    // for (let i = -1; i >= stringLengthAsNegative; i--) {
+    //     result += string.at(i);
+    // }
+    // return result;
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
