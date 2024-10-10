713155ec787bb752fc51f485bd4776813c766930
Chris <chrisn5413@gmail.com>
2024-10-10 10:55:06 -0700

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
 
+    let result = (count >= 0) ? "" : "ERROR";
+    
+    for (let i = 0; i < count; i++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
