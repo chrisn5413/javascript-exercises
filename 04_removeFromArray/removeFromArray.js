a34b443af239c5787416b5029fd2e6ec94d0cc8b
Chris <chrisn5413@gmail.com>
2024-10-10 12:33:37 -0700

    Completed exercise 4 by removing the array elements in place however I did not use ...args and instead used the arguments keyword

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    if (arguments.length == 1) return array;
+
+    let index = array.length;
+
+    while (index--) {
+        for (let i = 1; i < arguments.length; i++) {
+            if (array[index] === arguments[i]) {
+                array.splice(index, 1);
+                break;
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
