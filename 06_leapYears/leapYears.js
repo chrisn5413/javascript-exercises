5c20913eab6682e4540aa7376aaf699436eb9d90
Chris <chrisn5413@gmail.com>
2024-10-10 15:56:19 -0700

    completed exercise 6, original had if conditions, replaced with constants like used in solutions. It looks better.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const DivisibleByFour = year % 4 == 0;
+    const DivisibleByHundred = year % 100 == 0;
+    const DivisibleByFourHundred = year % 400 == 0;
+
+    if (DivisibleByFour && (!DivisibleByHundred || DivisibleByFourHundred)) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
