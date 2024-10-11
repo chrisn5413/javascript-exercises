b924d4ef5b6aea37e83b47285cab13d58ffc1db3
Chris <chrisn5413@gmail.com>
2024-10-10 18:10:32 -0700

    completed add, subtract, and sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,20 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0) return 0;
+
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  
+  return sum;
 };
 
 const multiply = function() {
