67285fe417e7cd9daac43d2cefa83f933f535944
Chris <chrisn5413@gmail.com>
2024-10-10 17:26:56 -0700

    completed exercise 7 rounded on separate line for clarity

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFahrenheit) {
+  let celcius = (degreesFahrenheit - 32) * 5 / 9;
+  let rounded = Math.round(celcius * 10) / 10;
+
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelcius) {
+  let fahrenheit = (degreesCelcius * 9 / 5) + 32;
+  let rounded = Math.round(fahrenheit * 10) / 10;
+
+  return rounded;
 };
 
 // Do not edit below this line
