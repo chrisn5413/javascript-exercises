115ca0684b6959a2bb788bd10f201108100f38b2
Chris <chrisn5413@gmail.com>
2024-10-10 18:18:54 -0700

    completed multiply, power, and factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,26 +7,34 @@ const subtract = function(num1, num2) {
 };
 
 const sum = function(array) {
-  if (array.length == 0) return 0;
-
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
     sum += array[i];
   }
-  
+
   return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let sum = 1;
+  for (let i = 0; i < array.length; i++) {
+    sum *= array[i];
+  }
 
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let sum = 1;
+  for (let i = 1; i <= number; i++) {
+    sum *= i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
