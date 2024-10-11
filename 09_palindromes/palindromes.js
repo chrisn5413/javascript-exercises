b28bfcf1cc2674c21a1bb949ad62f398862a7b3f
Chris <chrisn5413@gmail.com>
2024-10-10 18:57:22 -0700

    completed exerceise 9 used a loop comparing indexes instead of checking if reverse string is true

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let punctuation = [",",".","!"," "];
+    
+    word = word.toLowerCase().split("").filter((element) => !punctuation.includes(element));
 
+    let start = 0;
+    let end = word.length - 1;
+
+    while (start <= end) {
+        if (word[start] !== word[end]) return false;
+        start++;
+        end--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
