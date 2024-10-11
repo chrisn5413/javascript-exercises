1cc6a38f777c68ae76b0edceb6ac6de43786354e
Chris <chrisn5413@gmail.com>
2024-10-10 19:12:51 -0700

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(givenPosition) {
+    if (givenPosition < 0) return "OOPS";
+    if (givenPosition == 0) return 0;
+    if (givenPosition < 3) return 1;
 
+    let currentPosition = 3;
+    let secondPreviousNumber = 1;
+    let previousNumber = 1;
+    let currentNumber = secondPreviousNumber + previousNumber;
+
+    while (currentPosition < givenPosition) {
+        currentPosition++;
+        secondPreviousNumber = previousNumber;
+        previousNumber = currentNumber;
+        currentNumber = secondPreviousNumber + previousNumber;
+    }
+
+    return currentNumber;
 };
 
 // Do not edit below this line
